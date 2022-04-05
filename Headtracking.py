19f78ad5366805a47fb875b44edc6da1a0ba67b6
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-04-05 14:40:47 +0200

    Update Headtracking.py

--- Headtracking.py
+++ Headtracking.py
@@ -92,7 +92,7 @@ def main():
 
     port = host = None
     for match in service_matches:
-        if match["name"] == "GEARMANAGER" or match["name"] == b"GEARMANAGER":
+        if match["name"] == "GEARMANAGER" or match["name"] == b"GEARMANAGER" or match["name"] == b"FACTORY":
             port = match["port"]
             host = match["host"]
             break
